3d0ee1b978eb9b6cee1178d4e2cb988023998c39
blupandaman <blupandaman@pm.me>
2024-05-27 10:34:19 -0400

    chore: update example page

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,16 +1,26 @@
 import { checkAuth, getServerAuthSession } from "@/server/auth";
 import { SignOutButton } from "./_components/sign-out-button";
+import { api } from "@/trpc/server";
 
 export default async function AppHome() {
   await checkAuth();
 
   const session = await getServerAuthSession();
 
+  const hello = await api.example.hello({ text: "World" });
+  const secretMessage = await api.example.getSecretMessage();
+
   return (
     <main className="flex h-full flex-1 flex-col items-center justify-center gap-4 p-4">
+      <div className="rounded border p-4 text-center">
+        <p>{hello.greeting}</p>
+        <p>{secretMessage}</p>
+      </div>
+
       <pre className="rounded bg-muted p-4 shadow">
         {JSON.stringify(session, null, 2)}
       </pre>
+
       <SignOutButton />
     </main>
   );
